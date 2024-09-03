db298b0cb32e4e245074e44bad302abddab01ade
Murilo Sena <102439644+Sena2K@users.noreply.github.com>
2024-09-02 21:02:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class PokemonScrapper(scrapy.Spider):
             number = pokemon.css('td.cell-num span.infocard-cell-data::text').get()
             name = pokemon.css('td.cell-name a.ent-name::text').get()
             link = pokemon.css('td.cell-name a.ent-name::attr(href)').get()
-            pokemon_url = response.urljoin(link).replace('\\', '')  # Remove as barras invertidas
+            pokemon_url = response.urljoin(link)
 
             types = ", ".join(pokemon.css('td.cell-icon a.type-icon::text').getall())
 
@@ -46,16 +46,30 @@ class PokemonScrapper(scrapy.Spider):
         item['url'] = response.meta['url']
         item['types'] = response.meta['types']
 
-        # Captura altura e peso
         item['height_cm'] = response.css('.vitals-table tr:contains("Height") td::text').get().strip()
         item['weight_kg'] = response.css('.vitals-table tr:contains("Weight") td::text').get().strip()
 
-        # Captura evoluções
-        item['next_evolutions'] = self.extract_evolutions(response, item['name'])
+        evolutions = []
+        current_pokemon_found = False
+        for evo in response.css('.infocard-list-evo .infocard'):
+            evo_number = evo.css('.text-muted small::text').get()
+            evo_name = evo.css('.ent-name::text').get()
+            evo_link = response.urljoin(evo.css('a::attr(href)').get())
 
-        # Captura habilidades
+            if evo_name and evo_name.strip() == item['name'].strip():
+                current_pokemon_found = True
+            elif current_pokemon_found and evo_name and evo_number:
+                evo_number_int = int(evo_number.lstrip('#').lstrip('0') or '0')
+                evolutions.append({
+                    'number': evo_number_int,
+                    'name': evo_name,
+                    'url': evo_link
+                })
+
+        item['next_evolutions'] = evolutions
+
         ability_links = response.css('.vitals-table tr:contains("Abilities") td a::attr(href)').getall()
-        ability_links = [response.urljoin(link).replace('\\', '') for link in ability_links]  # Remove as barras invertidas
+        ability_links = [response.urljoin(link) for link in ability_links]
 
         if ability_links:
             request = Request(ability_links[0], callback=self.parse_ability, dont_filter=True)
@@ -68,33 +82,12 @@ class PokemonScrapper(scrapy.Spider):
             self.items.append(dict(item))
             yield item
 
-    def extract_evolutions(self, response, current_pokemon_name):
-        evolutions = []
-        current_pokemon_found = False
-
-        for evo in response.css('.infocard-list-evo .infocard'):
-            evo_number = evo.css('.text-muted small::text').get()
-            evo_name = evo.css('.ent-name::text').get()
-            evo_link = response.urljoin(evo.css('a::attr(href)').get()).replace('\\', '')  # Remove as barras invertidas
-
-            if evo_name and evo_name.strip() == current_pokemon_name.strip():
-                current_pokemon_found = True
-            elif current_pokemon_found and evo_name and evo_number:
-                evo_number_int = int(evo_number.lstrip('#').lstrip('0') or '0')
-                evolutions.append({
-                    'number': evo_number_int,
-                    'name': evo_name,
-                    'url': evo_link
-                })
-
-        return evolutions
-
     def parse_ability(self, response):
         ability_info = {
             'name': response.css('main > h1::text').get().strip(),
-            'desc': self.clean_html_tags(response.css('.vitals-table > tbody > tr:nth-child(1) > td::text').get()),
-            'effect': self.clean_html_tags(response.css('main > div > div > p').get()),
-            'url': response.css('link[rel="canonical"]::attr(href)').get().replace('\\', '')  # Remove as barras invertidas
+            'desc': response.css('.vitals-table > tbody > tr:nth-child(1) > td::text').get(),
+            'effect': response.css('main > div > div > p').get(),
+            'url': response.css('link[rel="canonical"]::attr(href)').get()
         }
 
         item = response.meta['item']
@@ -113,58 +106,52 @@ class PokemonScrapper(scrapy.Spider):
             self.items.append(dict(item))
             yield item
 
-    def clean_html_tags(self, text):
-        """Remove as tags HTML <p> e <em> e quaisquer outras tags HTML do texto."""
-        if text:
-            text = re.sub(r'<.*?>', '', text)  # Remove todas as tags HTML
-        return text
-
 def clean_measurement(value):
     if value:
         # Remove todos os caracteres que não são dígitos, pontos ou vírgulas
         cleaned_value = re.sub(r'[^\d.,]', '', value)
-        
-        # Tratar múltiplos pontos/vírgulas separando a string e mantendo a parte relevante
-        if ',' in cleaned_value and '.' in cleaned_value:
-            cleaned_value = cleaned_value.replace(',', '')
-
+        # Verifica se existem múltiplos pontos ou vírgulas e mantém apenas o último
+        parts = re.findall(r'\d+', cleaned_value)
+        if len(parts) > 1:
+            cleaned_value = '.'.join(parts[-2:])
+        else:
+            cleaned_value = parts[0]
         # Substitui vírgulas por pontos
         cleaned_value = cleaned_value.replace(',', '.')
-        
         try:
             return float(cleaned_value)
         except ValueError:
-            return None  # Retorna None se não for possível converter
+            return None  # Se não for possível converter, retorna None
     return None
 
 if __name__ == "__main__":
     process = CrawlerProcess()
 
-    # Executa o spider
+    # Run the spider
     process.crawl(PokemonScrapper)
     process.start()
 
-    # Ordena os Pokémon por ID antes de salvar
+    # Ordenar os Pokémon por ID antes de salvar
     PokemonScrapper.items.sort(key=lambda x: x['number'])
 
-    # Salva os resultados em um arquivo JSON
+    # Save the results to a JSON file
     with open('pokemons.json', 'w', encoding='utf-8') as f:
         json.dump(PokemonScrapper.items, f, ensure_ascii=False, indent=4)
 
-    # Carrega o JSON gerado pelo Scrapy
+    # Carregar o JSON gerado pelo Scrapy
     with open('pokemons.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-    # Converte para DataFrame
+    # Converter para DataFrame
     df = pd.DataFrame(data)
 
-    # Remove entradas com dados nulos ou inválidos
+    # Remover entradas com dados nulos ou inválidos
     df.dropna(inplace=True)
 
-    # Limpa e converte altura e peso para valores numéricos
+    # Limpar e converter altura e peso para valores numéricos
     df['height_cm'] = df['height_cm'].apply(lambda x: clean_measurement(x) * 100 if x else None)
     df['weight_kg'] = df['weight_kg'].apply(clean_measurement)
 
-    # Salva o DataFrame limpo em um novo arquivo JSON ou CSV
+    # Salvar o DataFrame limpo em um novo arquivo JSON ou CSV
     df.to_json('pokemons_cleaned.json', orient='records', indent=4, force_ascii=False)
     df.to_csv('pokemons_cleaned.csv', index=False)
